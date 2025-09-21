66f0aecf3c40bf31a2a17e81aa1995fd0f726750
yoursezan <arafath.sezan1999@gmail.com>
2025-09-21 18:54:34 +0600

    oop_Constructor

--- bin/oop/oop_Constructor.dart
+++ bin/oop/oop_Constructor.dart
@@ -12,5 +12,30 @@ main(){
   // 4. Inheritance
   // 5. Polymorphism
 
+  /// constructor , class er vitor e toiri kora hoy,
+  // constructor alada vabe call korar dorkar nei,
+  // jokhn e akta call er object create hobe tokhn e auto sei class er object auto call hoye jabe.
+
+  /// formula :  when we create a method into class and that methods name is same as its class name then it's a constructor
+
+  // constructor :    class className{ className(){.......} }
+
+
+ Mobile iPhone = Mobile('IPhone', 2025);  // create object and it auto call our constructor
+
+ // for call , $objectname.variableName
+  print('${iPhone.mobileName}, ${iPhone.year}');
+
+}
+
+class Mobile{
+  late String mobileName;
+  late int year;
+
+
+  // constructor
+  Mobile(this.mobileName, this.year){     // same as class name
+  print('I buy this phone.');
+  }
 
 }
